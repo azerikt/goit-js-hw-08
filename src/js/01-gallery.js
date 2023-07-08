a2fbc8fd86ee99ec2b0397f5f52caf14bab714b9
azerikt <azerikt@gmail.com>
2023-07-08 12:40:22 +0200

    update code

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const galleryMarkUp = document.querySelector('.gallery');
+
+const galleryEl = galleryItems
+  .map(
+    ({ preview, description, original }) =>
+      `<a class="gallery__item" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+  )
+  .join('');
+
+galleryMarkUp.insertAdjacentHTML('beforeend', galleryEl);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  captionPosition: 'bottom',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,12 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const TIME_KEY = 'videoplayer-current-time';
+TimeSet();
+function updateToTime({ second }) {
+  localStorage.setItem(TIME_KEY, second);
+}
+console.log(player);
