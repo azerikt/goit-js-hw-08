57a5e1d6ac56f1a2194aedb1634ddac6bdb7ad0b
azerikt <azerikt@gmail.com>
2023-07-08 13:34:55 +0200

    fix gallery and add functional video player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,19 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const CURRENT_TIME = 'videoplayer-current-time';
 
 const iframe = document.querySelector('iframe');
-
 const player = new Player(iframe);
+player.on('timeupdate', throttle(onPlay, 1000));
 
-const TIME_KEY = 'videoplayer-current-time';
-TimeSet();
-function updateToTime({ second }) {
-  localStorage.setItem(TIME_KEY, second);
+function onPlay({ seconds }) {
+  localStorage.setItem(CURRENT_TIME, seconds);
 }
-console.log(player);
+setCurrentTime();
+function setCurrentTime() {
+  if (!localStorage.getItem(CURRENT_TIME)) {
+    return;
+  }
+  player.setCurrentTime(localStorage.getItem(CURRENT_TIME));
+}
