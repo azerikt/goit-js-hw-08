f0bf9af56905382bb46de946e35d0eb6b9b911be
azerikt <azerikt@gmail.com>
2023-07-08 15:33:04 +0200

    add feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('form');
+const LOCAL_STORAGE_KEY = 'selectedFilters';
+
+initForm();
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const formData = new FormData(form);
+  formData.forEach((value, name) => console.log(value, name));
+  evt.currentTarget.reset();
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+}
+
+function onFormInput(evt) {
+  let persistedFilters = localStorage.getItem(LOCAL_STORAGE_KEY);
+  persistedFilters = persistedFilters ? JSON.parse(persistedFilters) : {};
+  persistedFilters[evt.target.name] = evt.target.value;
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(persistedFilters));
+}
+
+function initForm() {
+  let persistedFilters = localStorage.getItem(LOCAL_STORAGE_KEY);
+  if (persistedFilters) {
+    persistedFilters = JSON.parse(persistedFilters);
+    Object.entries(persistedFilters).forEach(([name, value]) => {
+      form.elements[name].value = value;
+    });
+  }
+}
